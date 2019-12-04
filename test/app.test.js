d2decff2d86f48d2b3b70ff3a851cc2e7da23be6
Michael Weedman <michael.weedman@gmail.com>
2019-12-04 09:07:31 -0700

    Initial testing outline

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const expect = require('mocha').expect;
+const supertest = require('supertest');
+
+const app = require('../app');
+
+describe('App', () => {
+  describe('GET /apps', () => {
+
+  });
+});
